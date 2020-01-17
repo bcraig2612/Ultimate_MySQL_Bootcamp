37a9fc642c56afbcaf9fcb2a5dc668e821cb7945
Brandon Craig <bcraig2612@gmail.com>
2020-01-17 16:35:32 -0700

    Attempting To Connect Node with mySQL

--- Section16/app.js
+++ Section16/app.js
@@ -1,13 +1,35 @@
-let faker = require("faker");
+const express = require("express");
+const mysql = require("mysql");
+const faker = require("faker");
 
-// console.log(faker.internet.email());
-// console.log(faker.date.past());
-// console.log(faker.address.city());
+// Create mySQL connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+  // database: "mysql_bootcamp_project_db"
+});
 
-function generateAddress() {
-  console.log(faker.address.streetAddress());
-  console.log(faker.address.city());
-  console.log(faker.address.state());
-}
+// Connect to mySQL db
+db.connect(error => {
+  if (error) {
+    throw error;
+  }
+  console.log("mySQL Connected!");
+});
 
-generateAddress();
+const app = express();
+
+//Create database
+app.get("/createdb", (request, response) => {
+  let sql = "CREATE DATABASE mysql_bootcamp_project_db";
+  db.query(sql, (error, result) => {
+    if (error) throw error;
+    console.log(result);
+    response.send("Database has been created!");
+  });
+});
+
+app.listen("3000", () => {
+  console.log("Server started on port 3000");
+});
