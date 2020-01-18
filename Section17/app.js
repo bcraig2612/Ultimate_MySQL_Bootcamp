7705e14ca58e814c06d47d56bdf61ba9a02ad2a0
Brandon Craig <bcraig2612@gmail.com>
2020-01-18 12:54:02 -0700

    Finished Section 16

--- Section17/app.js
+++ Section17/app.js
@@ -1,19 +1,41 @@
-var express = require("express");
-var app = express();
+let express = require("express");
+let mysql = require("mysql");
+let bodyParser = require("body-parser");
+let app = express();
+
+// Using EJS (Embedded JavaScript)
+app.set("view engine", "ejs");
+// Using body-parse to allow us to extract data from our Post Requests:
+app.use(bodyParser.urlencoded({ extended: true }));
+// Informing Express to take whatever is in the public folder & serve them where we want access to them
+// In this case, we link our css in home.ejs within the head
+app.use(express.static(__dirname + "/public"));
+
+// Create mySQL connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Kblair_2612",
+  database: "join_us"
+});
 
 app.get("/", function(request, response) {
-  response.send("Hello from our Web App!");
+  // Find count of users in DB
+  var q = "SELECT COUNT(*) AS count FROM users";
+  db.query(q, function(error, result) {
+    if (error) throw error;
+    let count = result[0].count;
+    // By adding EJS, this now looks for our home.ejs file:
+    response.render("home", { count: count });
+  });
 });
 
-app.get("/joke", function(request, response) {
-  let joke =
-    "What do you call a dog that does magic tricks? A labracadabrador.";
-  response.send(joke);
-});
-
-app.get("/random_num", function(request, response) {
-  let num = Math.floor(Math.random() * 10 + 1);
-  response.send("Your lucky number is " + num);
+app.post("/register", function(request, response) {
+  let person = { email: request.body.email };
+  db.query("INSERT INTO users SET ?", person, function(error, result) {
+    if (error) throw error;
+    response.redirect("/");
+  });
 });
 
 app.listen(3000, function() {
